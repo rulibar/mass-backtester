39c8e6649aa5665e473b3110143fd2fa9fc9a154
Ryan Ulibarri <38776589+rulibar@users.noreply.github.com>
2020-07-30 00:57:03 -0600

    Create maba.py

---
+++ maba.py
@@ -0,0 +1,36 @@
+"""
+Mass Backtester
+https://github.com/rulibar/mass-backtester
+"""
+
+import os
+
+# user vars
+data_path = "data/"
+
+# main code
+datasets = os.listdir(data_path)
+
+# cycle through the data
+for dataset in datasets:
+    """
+    - Import the data
+    - Set up the logger
+    - Initialize the Backtest instance
+    - Run the backtest
+    - Save the relevant data from the Backtest instance
+    """
+    #dsname = ds[:-4]
+    print(ds)
+
+    # Import data
+
+    # Set up the logger
+
+    # Initialize the Backtest instance
+
+    # Run the backtest
+
+    # Save the relevant data from the Backtest instance
+
+# create the summary
